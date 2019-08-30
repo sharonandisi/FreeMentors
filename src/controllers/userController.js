70488b28633724aef6e4023f0530c33e4a3222f8
sharonandisi <sharonandisi.28@gmail.com>
2019-08-30 15:11:37 +0300

    [#168211793]crete the userLogin function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
+import bcrypt from 'bcrypt';
 import UserModel from '../models/userModel';
+
 
 const User = {
   /**
@@ -19,6 +21,40 @@ const User = {
       status: 201,
       message: 'successful',
       data: user,
+    });
+  },
+
+  /**
+   * @param {object} req
+   * @param {object} res
+   * @returns {object} user object
+   */
+
+  userLogin(req, res) {
+    const user = UserModel.findByEmail(req.body.email);
+    if (!user) {
+      return res.status(404).json({
+        status: 404,
+        error: 'User not found'
+      });
+    }
+    if (req.body.password !== user.password) {
+      return res.status(401).json({
+        status: 401,
+        error: 'Invalid credentials',
+      });
+    }
+    return res.status(200).json({
+      status: 200,
+      message: 'user is successfully logged in',
+      data: {
+        id: user.id,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        email: user.email,
+        occupation: user.occupation,
+        mentorstatus: user.mentorstatus,
+      },
     });
   },
 };
