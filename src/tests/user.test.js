7ff7cc14e93e034c161a25bd251c5eb0ffde722b
sharonandisi <sharonandisi.28@gmail.com>
2019-08-30 14:46:50 +0300

    [#168211793]write more tests for the sign in feature

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -248,5 +248,21 @@ describe('/Auth', () => {
           done();
         });
     });
+
+    it('should not sign in a user missing a password', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/signin')
+        .send({
+          email: 'sharonandisi@gmail.com',
+          password: '',
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          expect(res.body.error)
+            .equals('Password is a required field with a min of 5 chars and no special chars');
+          if (err) return done();
+          done();
+        });
+    });
   });
 });
