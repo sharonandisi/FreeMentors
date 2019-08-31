62852c28169738e178b60ced865056bb29651a43
sharonandisi <sharonandisi.28@gmail.com>
2019-08-31 09:08:24 +0300

    [#168224421]create user model class methods

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -31,6 +31,29 @@ class User {
     this.users.push(newUser);
     return newUser;
   }
+
+  /**
+  * @returns {object} user object
+  */
+
+  createAdmin(data) {
+
+    const newUser = {
+      id: uuid.v4(),
+      firstname: data.firstname || '',
+      lastname: data.lastname || '',
+      email: data.email || '',
+      password: data.password || '',
+      address: data.address || '',
+      bio: data.bio || '',
+      occupation: data.occupation || '',
+      expertise: data.expertise || '',
+      mentorstatus: false,
+      is_Admin: true,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
   /**
    * @param {uuid} id
    * @returns {object} user object
@@ -42,6 +65,25 @@ class User {
 
   findByEmail(email) {
     return this.users.find((user) => user.email === email);
+  }
+
+  changeMentor(id) {
+    const user = this.findOne(id);
+    const index = this.users.indexOf(user);
+    this.users[index].mentorstatus = 'mentor';
+    return this.users[index];
+  }
+
+  findMentor() {
+    return this.users.filter((user) => user.mentorstatus === 'true');
+  }
+
+  remove() {
+    this.users = [];
+  }
+
+  findAdmin() {
+    return this.users.find((user) => user.is_Admin === 'true');
   }
 }
 export default new User();
