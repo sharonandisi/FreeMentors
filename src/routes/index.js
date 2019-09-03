9f5f980fc602d82904455f79fca67991e9963758
sharonandisi <sharonandisi.28@gmail.com>
2019-09-03 13:15:12 +0300

    [#starts 168259906] add the admin middleware to the base entrypoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import user from './userRoutes';
+import admin from '../middleware/admin';
 
 const router = express.Router();
-
+router.use('api/v1', admin);
 router.use('/api/v1/auth', user);
 
 export default router;
