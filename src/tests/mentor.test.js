441f96063979a75a6eb2ba68c374b156a5fb33d9
sharonandisi <sharonandisi.28@gmail.com>
2019-09-04 16:05:30 +0300

    [#168243175]add tests

--- src/tests/mentor.test.js
+++ src/tests/mentor.test.js
@@ -51,94 +51,51 @@ describe('/GET  all mentors', () => {
     });
 });
 
-describe('GET /api/v1/mentors:mentorId', () => {
+describe('GET /api/v1/mentors:mentorid', () => {
     beforeEach(() => {
         User.remove();
     });
-
-    let token = '';
     let mentorid = '';
+    let token = '';
 
     const execute = () => request(app)
-        .get(`/api/v1/mentors/${mentorid}`)
-        .set('x-auth-token', token);
-
-    it('should not get a specific mentor if the user has no token', async () => {
-        const user = User.create({ ...data.user001 });
-        const mentor = User.changeMentor(user.id);
+          .get(`/api/v1/mentors${mentorid}`)
+          .set('x-auth-token', token);
+    
+    it('should not get a mentor if the user making the request has no token', async () =>{
         token = '';
-        mentorid = mentor.id;
         const res = await execute();
         expect(res).to.have.status(401);
     });
 
-    it('shouldnt get a mentor when user has an invalid token', async () => {
-        const user = User.create({ ...data.user001 });
-        const mentor = User.changeMentor(user.id);
-        token = 'sigh';
-        mentorid = mentor.id;
+    it('should not get a mentor if the user has an invalid token', async () => {
+        token = 'sha23563rwe';
         const res = await execute();
         expect(res).to.have.status(401);
-    });
 
-    it('should throw error if mentor does not exist', async () => {
-        const user = User.create({ ...data.user001 });
-        const mentor = User.changeMentor(user.id);
-        token = 'sellah';
-        mentorid = '1002';
+    })
+
+    it('should throw an error if there are no mentors found', async () => {
+        const user = User.create({ ...testdata.user001 });
+        token = authHelper.generateToken(user.id);
         const res = await execute();
         expect(res).to.have.status(404);
-    });
+    })
 
-    it('should get a specific mentor if user is authenticated', async () => {
-        const user = User.create({ ...data.user001 });
+    it('should throw an error if there is no mentor found', async () => {
+        const user = User.create({ ...testdata.user001 });
         token = authHelper.generateToken(user.id);
-        const user =User.changeMentor(user.id);
-        mentorid = user.id;
+        const res = await execute();
+        expect(res).to.have.status(404);
+    })
+
+    it('should get a mentor if user is authenticated', async () => {
+        const user = User.create({ ...testdata.user001 });
+        token = authHelper.generateToken(user.id);
+        User.changeMentor(user.id);
 
         const res = await execute();
         expect(res).to.have.status(200);
     });
+
 });
-
-describe('GET /api/v1/mentors:mentorId', () => {
-    beforeEach(() => {
-        User.remove();
-    });
-
-    let token = '';
-    let mentorid = '';
-
-    const execute = () => request(app)
-        .get(`/api/v1/mentors/${mentorid}`)
-        .set('x-auth-token', token);
-
-    it('should not get a specific mentor if the user has no token', async () => {
-        const user = User.create({ ...data.user001 });
-        const mentor = User.changeMentor(user.id);
-        token = '';
-        mentorid = mentor.id;
-        const res = await execute();
-        expect(res).to.have.status(401);
-    });
-
-    it('should throw error if mentor does not exist', async () => {
-        const user = User.create({ ...data.user001 });
-        const mentor = User.changeMentor(user.id);
-        token = 'sellah';
-        mentorid = '1002';
-        const res = await execute();
-        expect(res).to.have.status(404);
-    });
-
-    it('should get a specific mentor if user is authenticated', async () => {
-        const user = User.create({ ...data.user001 });
-        token = authHelper.generateToken(user.id);
-        const user = User.changeMentor(user.id);
-        mentorid = user.id;
-
-        const res = await execute();
-        expect(res).to.have.status(200);
-    });
-});
-
