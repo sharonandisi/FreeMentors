cb8c47a55dc95c036d642f17618532799ece3240
sharonandisi <sharonandisi.28@gmail.com>
2019-09-04 13:49:21 +0300

    [Delivers #168243086]update the index.js file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import user from './userRoutes';
-import 
-
+import mentor from './mentorRoutes';
 
 const router = express.Router();
+
 router.use('/api/v1/auth', user);
-
-
+router.use('/api/v1/', mentor)
 export default router;
