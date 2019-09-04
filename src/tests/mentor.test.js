39fe9260811d12225bb931665ff1951c7bfde0bf
sharonandisi <sharonandisi.28@gmail.com>
2019-09-04 14:52:10 +0300

    [start #168243175]create view a mentor tests

--- src/tests/mentor.test.js
+++ src/tests/mentor.test.js
@@ -1,6 +1,5 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import bcrypt from 'bcrypt';
 import app from '../../server';
 import userModel from '../models/userModel';
 import '../../config';
@@ -44,8 +43,57 @@ describe('/GET  all mentors', () => {
 
     it('should get mentors in the app if the user is authenticated', async () => {
         const user = User.create({ ...testdata.user001 });
+        token = authHelper.generateToken(user.id);
+        User.changeRole(user.id);
+
+        const res = await exec();
+        expect(res).to.have.status(200);
+    });
+});
+
+describe('GET /api/v1/mentors:mentorId', () => {
+    beforeEach(() => {
+        User.remove();
+    });
+
+    let token = '';
+    let mentorId = '';
+
+    const exec = () => request(app)
+        .get(`/api/v1/mentors/${mentorId}`)
+        .set('x-auth-token', token);
+
+    it('should not fetch a specific mentor if the user has no token', async () => {
+        const user = User.create({ ...data.user001 });
+        const mentor = User.changeMentor(user.id);
+        token = '';
+        mentorId = mentor.id;
+        const res = await exec();
+        expect(res).to.have.status(401);
+    });
+
+    it('should not fetch a specific mentor if the user has an invalid token', async () => {
+        const user = User.create({ ...data.user001 });
+        const mentor = User.changeMentor(user.id);
+        token = 'yes';
+        mentorId = mentor.id;
+        const res = await exec();
+        expect(res).to.have.status(401);
+    });
+
+    it('should notify a user if the mentor does not exist', async () => {
+        const user = User.create({ ...data.user001 });
+        token = User.changeMentor(user.id);
+        mentorId = '5';
+        const res = await exec();
+        expect(res).to.have.status(404);
+    });
+
+    it('should fectch a specific mentor in the app if the user is authenticated', async () => {
+        const user = User.create({ ...data.user001 });
         token = generateToken(user.id);
         User.changeRole(user.id);
+        mentorId = user.id;
 
         const res = await exec();
         expect(res).to.have.status(200);
