5e57895170fe7002753c68068b0c81140a97ed95
sharonandisi <sharonandisi.28@gmail.com>
2019-09-04 13:49:48 +0300

    [Delivers #168243086]update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,18 +5,6 @@ module.exports = {
             "es6": true
     },
     "extends": "airbnb-base",
-    //     "globals": {
-    //     "Atomics": "readonly",
-    //         "SharedArrayBuffer": "readonly",
-    //             "describe": "readonly",
-    //                 "it": "readonly",
-    //                     "before": "readonly",
-    //                         "after": "readonly"
-    // },
-    // "parserOptions": {
-    //     "ecmaVersion": 2018,
-    //         "sourceType": "module"
-    // },
     "rules": {
         "comma-dangle": "warn",
         "import/no-named-as-default": 0 
