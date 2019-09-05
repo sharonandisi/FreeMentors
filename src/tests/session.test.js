cf2e8558468189ed6649c262d17bfc4069582c8f
sharonandisi <sharonandisi.28@gmail.com>
2019-09-05 15:32:30 +0300

    [#168243546]write tests for decline a request

--- src/tests/session.test.js
+++ src/tests/session.test.js
@@ -34,7 +34,7 @@ describe('POST /api/v1/sessions', () => {
 
 });
 
-describe ('PATCH /api/v1/sessions/:sessionid', () => {
+describe ('PATCH /api/v1/sessions/:sessionid/accept', () => {
     beforeEach(() => {
         User.remove();
         Session.remove();
@@ -44,7 +44,7 @@ describe ('PATCH /api/v1/sessions/:sessionid', () => {
     let token = '';
 
     const execute = () => request(app)
-        .post('/api/v1/sessions')
+        .patch('/api/v1/sessions/sessions/:sessionid/accept')
         .send(session)
         .set('x-auth-token', token);
 
@@ -61,6 +61,33 @@ describe ('PATCH /api/v1/sessions/:sessionid', () => {
         const res = await execute();
         expect(res).to.have.status(200);
     });
+});
 
-    
+describe('PATCH /api/v1/sessions/:sessionid/reject', () => {
+    beforeEach(() => {
+        User.remove();
+        Session.remove();
+    });
+
+    let session = {};
+    let token = '';
+
+    const execute = () => request(app)
+        .patch('/api/v1/sessions/sessions/:sessionid/reject')
+        .send(session)
+        .set('x-auth-token', token);
+
+    it('should allow a mentor to accept a session ', async () => {
+        const { id: mentorid } = User.create({ ...testdata.user001 });
+        User.changeMentor(mentorid);
+        token = authHelper.generateToken(mentorid);
+
+        const { id: menteeid } = User.create({ ...testdata.user001 });
+        const { questions } = testdata.session001;
+        const newSession = Session.create({ mentorid, menteeid, questions });
+
+        sessionid = newSession.id;
+        const res = await execute();
+        expect(res).to.have.status(200);
+    });
 });
